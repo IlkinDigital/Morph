173c213e68d25a0a3530940debfd8fce93b95faf
Ilkin Mammadli <81753493+IlkinDigital@users.noreply.github.com>
2022-10-03 22:32:31 -0400

    Seperated Includes with workspace premake file.

---
+++ Vendor.lua
@@ -0,0 +1,17 @@
+Include = {}
+Include["Morph"] = "%{wks.location}/Morph/Source"
+Include["GLFW"] = "%{wks.location}/ThirdParty/GLFW/include"
+Include["glad"] = "%{wks.location}/ThirdParty/glad/include"
+Include["ImGui"] = "%{wks.location}/ThirdParty/ImGui"
+
+Library = {}
+Library["GLFW"] = "GLFW"
+Library["glad"] = "glad"
+Library["ImGui"] = "ImGui"
+
+include "ThirdParty/GLFW"
+include "ThirdParty/glad"
+include "ThirdParty/ImGui"
+
+include "Morph"
+include "MorphApp"
--- premake5.lua
+++ premake5.lua
@@ -11,20 +11,4 @@ workspace "Morph"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-Include = {}
-Include["Morph"] = "%{wks.location}/Morph/Source"
-Include["GLFW"] = "%{wks.location}/ThirdParty/GLFW/include"
-Include["glad"] = "%{wks.location}/ThirdParty/glad/include"
-Include["ImGui"] = "%{wks.location}/ThirdParty/ImGui"
-
-Library = {}
-Library["GLFW"] = "GLFW"
-Library["glad"] = "glad"
-Library["ImGui"] = "ImGui"
-
-include "ThirdParty/GLFW"
-include "ThirdParty/glad"
-include "ThirdParty/ImGui"
-
-include "Morph"
-include "MorphApp"
+include "Vendor.lua"
